ab0e541836713568946700c6bae5f0ca2ade8a30
kartikgrover <groverkartik2000@yahoo.com>
2023-10-13 18:47:50 -0400

    simplified timeout

--- content.js
+++ content.js
@@ -6,53 +6,45 @@ function addNoteAndMessage() {
   const addNoteButton = document.querySelector("button[aria-label='Add a note']");
   if (addNoteButton) {
     addNoteButton.click();
-
-    // Wait for the "Add a note" dialog to open (you may need to implement this part).
-
-    // Set the personalized note in the "custom-message" textarea when it becomes available.
-    const waitForTextArea = setInterval(() => {
-      const textArea = document.getElementById("custom-message");
-      if (textArea) {
-        textArea.value = personalizedNote;
-        clearInterval(waitForTextArea);
-      }
-    }, 100); // Adjust the interval if necessary.
   } else {
     console.error("Add a note button not found.");
   }
 }
 
-// Set up a MutationObserver to handle the "Connect" button click event.
-const observer = new MutationObserver(function (mutations) {
-  mutations.forEach(function (mutation) {
-    const buttons = Array.from(mutation.addedNodes).filter((node) => {
-      return (
-        node.nodeType === Node.ELEMENT_NODE &&
-        node.querySelector('.artdeco-button__text')?.textContent.trim() === 'Connect'
-      );
-    });
+// Function to handle the "Connect" button click.
+function handleConnectButtonClick() {
+  // Get the person's name from the LinkedIn profile.
+  const nameElement = document.querySelector('span.flex-1 strong');
+  if (nameElement) {
+    const personName = nameElement.textContent;
+    const firstName = personName.split(" ")[0];
 
-    buttons.forEach(function (button) {
-      button.addEventListener("click", function () {
-        // Get the person's name from the LinkedIn profile.
-        const nameElement = document.querySelector('span.flex-1 strong');
-        if (nameElement) {
-          const personName = nameElement.textContent;
-          firstName = personName.split(" ")[0];
+    // Set the personalized note.
+    personalizedNote = `Hi ${firstName},\nI came across your profile and noticed that you are a fellow Penn Stater. The reason I am reaching out to you is that I had recently applied for the Software Engineer 1 position at Microsoft and I would love to follow your insights on how I can remain a competitive applicant.`;
 
-          // Set the personalizedNote variable
-          personalizedNote = `Hi ${firstName},\nI came across your profile and noticed that you are a fellow Penn Stater. The reason I am reaching out to you today is that I had recently applied for the Software Engineer 1 position at Microsoft and I would love to follow your insights on how I can remain a competitive applicant.`;
+    // Execute the addNoteAndMessage function immediately.
+    addNoteAndMessage();
+  }
+}
 
-          // Execute the addNoteAndMessage function immediately.
-          addNoteAndMessage();
-        }
-      });
-    });
-  });
+// Function to wait for the "custom-message" textarea to become available.
+function waitForTextArea() {
+  const textArea = document.getElementById("custom-message");
+  if (textArea) {
+    textArea.value = personalizedNote;
+  } else {
+    setTimeout(waitForTextArea, 100); // Retry after 100ms
+  }
+}
+
+// Set up a click event listener for the "Connect" button.
+document.addEventListener("click", function (event) {
+  const target = event.target;
+  if (target && target.textContent.trim() === "Connect") {
+    handleConnectButtonClick();
+    waitForTextArea(); // Start waiting for the textarea
+  }
 });
 
-const targetNode = document.body;
-const config = { childList: true, subtree: true };
-
-// Start observing the body for changes.
-observer.observe(targetNode, config);
+// You may continue observing the body for changes if needed.
+// ...
