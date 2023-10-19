9564fba25ab2559a88351e4707ffd5bdd55eba58
kartikgrover <groverkartik2000@yahoo.com>
2023-10-19 16:38:36 -0400

    added the send button without timeout

--- content.js
+++ content.js
@@ -2,7 +2,7 @@
 let personalizedNote = "";
 
 // Function to click the "Add note" button and add your note.
-function addNoteAndMessage() {
+function clickAddNoteButton() {
   const addNoteButton = document.querySelector("button[aria-label='Add a note']");
   if (addNoteButton) {
     addNoteButton.click();
@@ -11,19 +11,15 @@ function addNoteAndMessage() {
   }
 }
 
-
-// test
-// Function to click the "Send now" button with a timeout.
-function clickSendButtonWithTimeout() {
-  setTimeout(() => {
-    // Select the "Send now" button within the div with the class 'artdeco-modal__actionbar'
-    const sendButton = document.querySelector(".artdeco-modal__actionbar button[aria-label='Send now']");
-    if (sendButton) {
-      sendButton.click();
-    } else {
-      console.error("Send button not found.");
-    }
-  }, 10000); // Adjust the timeout value as needed
+// Function to click the "Send now" button.
+function clickSendNowButton() {
+  // Select the "Send now" button within the div with the class 'artdeco-modal__actionbar'
+  const sendButton = document.querySelector("button[aria-label='Send now']");
+  if (sendButton) {
+    sendButton.click();
+  } else {
+    console.error("Send now button not found.");
+  }
 }
 
 // Function to handle the "Connect" button click.
@@ -35,13 +31,12 @@ function handleConnectButtonClick() {
     const firstName = personName.split(" ")[0];
 
     // Set the personalized note.
-    personalizedNote = `Hi ${firstName},\nI came across your profile and noticed that you work at Loop. The reason I am reaching out to you is that I had recently applied for the Software Engineer 1 position there and I would love to follow your insights on how I can remain a competitive applicant.`;
-
-    // Execute the addNoteAndMessage function immediately.
-    addNoteAndMessage();
-
-    // Call the function to click the "Send now" button with a timeout
-    clickSendButtonWithTimeout();
+    personalizedNote = `Hi ${firstName},\nI came across your profile and noticed that you work at Loop. The reason I'm reaching out to you is that I recently applied for the Software Engineer 1 position there, and I'd love to follow your insights on how I can remain a competitive applicant.`;
+    
+    // Click the "Add note" button to open the note dialog.
+    clickAddNoteButton();
+    // Start waiting for the "custom-message" textarea.
+    waitForTextArea();
   }
 }
 
@@ -50,8 +45,12 @@ function waitForTextArea() {
   const textArea = document.getElementById("custom-message");
   if (textArea) {
     textArea.value = personalizedNote;
+    textArea.blur(); // Remove focus from the textarea to enable the "Send now" button.
+    // Click the "Send now" button after setting the personalized note.
+    clickSendNowButton();
   } else {
-    setTimeout(waitForTextArea, 500); // Retry after 500ms
+    // If the textarea is not available yet, retry after a short delay.
+    setTimeout(waitForTextArea, 150);
   }
 }
 
@@ -60,6 +59,5 @@ document.addEventListener("click", function (event) {
   const target = event.target;
   if (target && target.textContent.trim() === "Connect") {
     handleConnectButtonClick();
-    waitForTextArea(); // Start waiting for the textarea
   }
 });
