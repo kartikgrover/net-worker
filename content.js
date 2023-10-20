d6430446090182cdc9808f40b408f4fcfb987d31
kartikgrover <groverkartik2000@yahoo.com>
2023-10-19 23:11:55 -0400

    impleted async to wait for custom-message

--- content.js
+++ content.js
@@ -1,27 +1,29 @@
-// Define a variable to hold the personalized note.
 let personalizedNote = "";
 
-// Function to click an element based on a CSS selector.
 function clickElement(selector, errorMessage) {
   const element = document.querySelector(selector);
-  if (element) {
-    element.click();
-  } else {
+  if (!element) {
     console.error(errorMessage);
+    return;
   }
+  element.click();
 }
 
-// Function to wait for an element to become available.
-function waitForElement(selector, callback, delay = 150) {
-  const element = document.querySelector(selector);
-  if (element) {
-    callback(element);
-  } else {
-    setTimeout(() => waitForElement(selector, callback, delay), delay);
-  }
+function waitForElement(selector) {
+  return new Promise((resolve) => {
+    const checkInterval = 50; // Reduced the interval
+    const checkElement = () => {
+      const element = document.querySelector(selector);
+      if (element) {
+        resolve(element);
+      } else {
+        setTimeout(checkElement, checkInterval);
+      }
+    };
+    checkElement();
+  });
 }
 
-// Function to set the personalized note based on the user's name.
 function setPersonalizedNote() {
   const nameElement = document.querySelector('span.flex-1 strong');
   if (nameElement) {
@@ -31,34 +33,19 @@ function setPersonalizedNote() {
   }
 }
 
-// Function to handle the "Connect" button click.
-function handleConnectButtonClick() {
-  // Set the personalized note.
+async function handleConnectButtonClick() {
   setPersonalizedNote();
-
-  // Click the "Add note" button to open the note dialog.
   clickElement("button[aria-label='Add a note']", "Add a note button not found");
-
-  // Start waiting for the "custom-message" textarea.
-  waitForElement("#custom-message", (textArea) => {
-    textArea.value = personalizedNote;
-
-    // Use a setTimeout before triggering the blur event.
-    setTimeout(() => {
-      textArea.blur();
-
-      // After a brief delay, click the "Send now" button.
-      setTimeout(() => clickSendNowButton(), 0); // Adjust the delay as needed
-    }, 0); // Adjust the delay as needed
-  });
+  const textArea = await waitForElement("#custom-message");
+  textArea.value = personalizedNote;
+  setTimeout(() => textArea.blur(), 0);
+  setTimeout(() => clickSendNowButton(), 0); // Adjust the delay as needed
 }
 
-// Function to click the "Send now" button.
 function clickSendNowButton() {
   clickElement("button[aria-label='Send now']", "Send now button not found");
 }
 
-// Set up a click event listener for the "Connect" button.
 document.addEventListener("click", function (event) {
   const target = event.target;
   if (target && target.textContent.trim() === "Connect") {
