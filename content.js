f16bb040594da60ebce60b19667586c25c61dd41
kartikgrover <groverkartik2000@yahoo.com>
2023-10-19 22:56:54 -0400

    changed timeouts to fix bugs

--- content.js
+++ content.js
@@ -1,57 +1,61 @@
 // Define a variable to hold the personalized note.
 let personalizedNote = "";
 
-// Function to click the "Add note" button and add your note.
-function clickAddNoteButton() {
-  const addNoteButton = document.querySelector("button[aria-label='Add a note']");
-  if (addNoteButton) {
-    addNoteButton.click();
+// Function to click an element based on a CSS selector.
+function clickElement(selector, errorMessage) {
+  const element = document.querySelector(selector);
+  if (element) {
+    element.click();
   } else {
-    console.error("Add a note button not found.");
+    console.error(errorMessage);
   }
 }
 
-// Function to click the "Send now" button.
-function clickSendNowButton() {
-  // Select the "Send now" button within the div with the class 'artdeco-modal__actionbar'
-  const sendButton = document.querySelector("button[aria-label='Send now']");
-  if (sendButton) {
-    sendButton.click();
+// Function to wait for an element to become available.
+function waitForElement(selector, callback, delay = 150) {
+  const element = document.querySelector(selector);
+  if (element) {
+    callback(element);
   } else {
-    console.error("Send now button not found.");
+    setTimeout(() => waitForElement(selector, callback, delay), delay);
+  }
+}
+
+// Function to set the personalized note based on the user's name.
+function setPersonalizedNote() {
+  const nameElement = document.querySelector('span.flex-1 strong');
+  if (nameElement) {
+    const personName = nameElement.textContent;
+    const firstName = personName.split(" ")[0];
+    personalizedNote = `Hi ${firstName},\nI came across your profile and noticed that you work at Crusoe. The reason I'm reaching out to you is that I recently applied for the Software Engineer 1 position there, and I'd love to follow your insights on how I can remain a competitive applicant.`;
   }
 }
 
 // Function to handle the "Connect" button click.
 function handleConnectButtonClick() {
-  // Get the person's name from the LinkedIn profile.
-  const nameElement = document.querySelector('span.flex-1 strong');
-  if (nameElement) {
-    const personName = nameElement.textContent;
-    const firstName = personName.split(" ")[0];
+  // Set the personalized note.
+  setPersonalizedNote();
 
-    // Set the personalized note.
-    personalizedNote = `Hi ${firstName},\nI came across your profile and noticed that you work at Loop. The reason I'm reaching out to you is that I recently applied for the Software Engineer 1 position there, and I'd love to follow your insights on how I can remain a competitive applicant.`;
-    
-    // Click the "Add note" button to open the note dialog.
-    clickAddNoteButton();
-    // Start waiting for the "custom-message" textarea.
-    waitForTextArea();
-  }
+  // Click the "Add note" button to open the note dialog.
+  clickElement("button[aria-label='Add a note']", "Add a note button not found");
+
+  // Start waiting for the "custom-message" textarea.
+  waitForElement("#custom-message", (textArea) => {
+    textArea.value = personalizedNote;
+
+    // Use a setTimeout before triggering the blur event.
+    setTimeout(() => {
+      textArea.blur();
+
+      // After a brief delay, click the "Send now" button.
+      setTimeout(() => clickSendNowButton(), 0); // Adjust the delay as needed
+    }, 0); // Adjust the delay as needed
+  });
 }
 
-// Function to wait for the "custom-message" textarea to become available.
-function waitForTextArea() {
-  const textArea = document.getElementById("custom-message");
-  if (textArea) {
-    textArea.value = personalizedNote;
-    textArea.blur(); // Remove focus from the textarea to enable the "Send now" button.
-    // Click the "Send now" button after setting the personalized note.
-    clickSendNowButton();
-  } else {
-    // If the textarea is not available yet, retry after a short delay.
-    setTimeout(waitForTextArea, 150);
-  }
+// Function to click the "Send now" button.
+function clickSendNowButton() {
+  clickElement("button[aria-label='Send now']", "Send now button not found");
 }
 
 // Set up a click event listener for the "Connect" button.
